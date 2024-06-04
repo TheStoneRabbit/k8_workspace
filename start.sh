f958b262ffc18c276845f79c9398fec5d8954525
mlapine <mlapine@360privacy.io>
2024-06-04 15:41:06 -0500

    kubernetese persistence set

--- start.sh
+++ start.sh
@@ -4,6 +4,8 @@ minikube start
 eval $(minikube docker-env)
 minikube image load k8-masonic-db
 minikube image load redis-example-masonic
+kubectl apply -f kubernetes/pv.yaml
+kubectl apply -f kubernetes/pvc.yaml
 kubectl apply -f kubernetes/namespace.yaml
 kubectl apply -f kubernetes/db-deployment.yaml
 kubectl apply -f kubernetes/webapp-deployment.yaml
