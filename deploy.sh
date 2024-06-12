f5334d711bf9bedb804d741d2028d315ed07228c
mlapine <mlapine@360privacy.io>
2024-06-12 10:56:25 -0500

    readme update

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
 set -o xtrace
-# minikube start
 docker build -f docker/python/Dockerfile -t k8-masonic-webapp docker/python/
 docker build -f docker/redis/Dockerfile -t k8-masonic-db docker/redis/
-# eval $(minikube docker-env)
-# minikube image load k8-masonic-webapp
-# minikube image load k8-masonic-db
 kubectl apply -f kubernetes/namespace.yaml
 kubectl apply -f kubernetes/pv.yaml
 kubectl apply -f kubernetes/pvc.yaml
 kubectl apply -f kubernetes/db-deployment.yaml
 kubectl apply -f kubernetes/webapp-deployment.yaml
-# minikube tunnel
-# minikube stop
-# minikube delete
 kubectl rollout restart deployment -n masonic-app
 docker-compose up -d
 set +o xtrace
